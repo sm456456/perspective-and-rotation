2a371496caf2040d128d269930ab5095b4dc0275
Selma456 <74097172+sm456456@users.noreply.github.com>
2021-08-09 11:49:05 +0200

    add rotation

--- app.js
+++ app.js
@@ -6,5 +6,25 @@ Vue.createApp({
       rotateY: 0,
       rotateZ: 0
     }
+  },
+  computed: {
+    box() {
+      return {
+        transform: `
+          perspective(${this.perspective}px)
+          rotateX(${this.rotateX}deg)
+          rotateY(${this.rotateY}deg)
+          rotateZ(${this.rotateZ}deg)
+        `
+      }
+    }
+  },
+  methods: {
+    reset() {
+      this.perspective = 100
+      this.rotateX = 0
+      this.rotateY = 0
+      this.rotateZ = 0
+    }
   }
 }).mount("#app")
